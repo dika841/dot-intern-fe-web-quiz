4dcd8d7ac8c5284a5faffb4ec957c7ac96545610
dika841 <randikaa1999@gmail.com>
2024-10-11 00:08:56 +0700

    chores:change text total question  in result page to question answered

--- components/organisms/controlled-fields/controlled-field-text/controlled-field-text.tsx
+++ components/organisms/controlled-fields/controlled-field-text/controlled-field-text.tsx
@@ -1,8 +1,7 @@
-
-import { FieldText } from '@/components/molecules';
-import { TControlledInput } from '@/entities';
-import { ReactElement, forwardRef } from 'react';
-import { FieldValues, useController } from 'react-hook-form';
+import { FieldText } from "@/components/molecules";
+import { TControlledInput } from "@/entities";
+import { ReactElement } from "react";
+import { FieldValues, useController } from "react-hook-form";
 
 export const ControlledFieldText = <T extends FieldValues>(
   props: TControlledInput<T>
